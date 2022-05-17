86355b93de5c979fc49d9acb5e88f16b72811c34
NguyenTrung-PM <96853789+NguyenTrung-PM@users.noreply.github.com>
2022-05-17 22:27:51 +0700

    Finish feature header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/shared/header/header.component';
 import { FooterComponent } from './components/shared/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
-import { FormsModule } from '@angular/forms';
+
+import { MenubarModule } from 'primeng/menubar';
+import { SharedModule } from 'primeng/api';
 
 const COMPONENTS = [AppComponent, HeaderComponent, FooterComponent, HomeComponent];
 
-const PRIMENGS = [];
+const PRIMENGS = [SharedModule, MenubarModule];
 
 const MODULES = [BrowserModule, AppRoutingModule, FormsModule];
 
 @NgModule({
     declarations: [...COMPONENTS],
-    imports: [...MODULES],
+    imports: [...MODULES, ...PRIMENGS],
     providers: [],
     bootstrap: [AppComponent],
 })
