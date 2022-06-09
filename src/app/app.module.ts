c7f5cfd34779389dde0893bf4c19af88d8360e5a
vietprao2020 <deptraiphaithe258@gmail.com>
2022-06-09 14:01:30 +0700

    create login and profile components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MenubarModule } from 'primeng/menubar';
 import { SharedModule } from 'primeng/api';
 import { CarouselModule } from 'primeng/carousel';
 import { PanelMenuModule } from 'primeng/panelmenu';
+import { TableModule } from 'primeng/table';
 //component
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -17,15 +18,17 @@ import { FooterComponent } from './components/shared/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { HomeCarouselComponent } from './components/shared/home-carousel/home-carousel.component';
 import { ProductSidebarComponent } from './components/shared/product-sidebar/product-sidebar.component';
+import { LoginComponent } from './components/account/login/login.component';
+import { ProfileComponent } from './components/account/profile/profile.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 const COMPONENTS = [AppComponent, HeaderComponent, FooterComponent, HomeComponent];
 
-const PRIMENGS = [SharedModule, MenubarModule, InputTextModule, ButtonModule, CarouselModule, PanelMenuModule];
+const PRIMENGS = [SharedModule, MenubarModule, InputTextModule, ButtonModule, CarouselModule, PanelMenuModule, TableModule];
 
-const MODULES = [BrowserModule, BrowserAnimationsModule, AppRoutingModule, FormsModule, ReactiveFormsModule];
+const MODULES = [BrowserModule, BrowserAnimationsModule, AppRoutingModule, FormsModule, ReactiveFormsModule, ScrollingModule];
 
 @NgModule({
-    declarations: [...COMPONENTS, HomeCarouselComponent, ProductSidebarComponent],
-
+    declarations: [...COMPONENTS, HomeCarouselComponent, ProductSidebarComponent, LoginComponent, ProfileComponent],
     imports: [...MODULES, ...PRIMENGS],
     providers: [],
     bootstrap: [AppComponent],
